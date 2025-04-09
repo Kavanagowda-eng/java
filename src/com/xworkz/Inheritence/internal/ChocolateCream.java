878b9357dbc74191c8f3f5d8ae09be9dea6d72aa
kavana <kavanakavana72549@gmail.com>
2025-04-09 08:05:22 +0530

    override

--- src/com/xworkz/Inheritence/internal/ChocolateCream.java
+++ src/com/xworkz/Inheritence/internal/ChocolateCream.java
@@ -4,9 +4,37 @@ import com.xworkz.Inheritence.External.Cream;
 
 public class ChocolateCream extends Cream {
 
-        public ChocolateCream() {
-            super();
-            System.out.println("ChocolateCream is a subclass");
-        }
+    public ChocolateCream() {
+        super();
+        System.out.println("ChocolateCream is a subclass");
+    }
 
+    @Override
+    public void applyCream() {
+        super.applyCream();
+    }
+
+    @Override
+    public void soothingEffect() {
+        super.soothingEffect();
+    }
+
+    @Override
+    public void scent() {
+        super.scent();
+    }
+
+    @Override
+    public void provideMoisture() {
+        super.provideMoisture();
+    }
+
+    @Override
+    public void packaging() {
+        super.packaging();
+    }
+
+    public void color() {
+        System.out.println("color of the cream");
+    }
 }
--- src/com/xworkz/Inheritence/internal/Classmate.java
+++ src/com/xworkz/Inheritence/internal/Classmate.java
@@ -3,7 +3,36 @@ package com.xworkz.Inheritence.internal;
 import com.xworkz.Inheritence.External.Books;
 
 public class Classmate extends Books {
-    public Classmate(){
+    public Classmate() {
         System.out.println("No args const in Classmate");
+    }
+
+    @Override
+    public void size() {
+        super.size();
+    }
+
+    @Override
+    public void pages() {
+        super.pages();
+    }
+
+    @Override
+    public void print() {
+        super.print();
+    }
+
+    @Override
+    public void read() {
+        super.read();
+    }
+
+    @Override
+    public void type() {
+        super.type();
+    }
+
+    public void color() {
+        System.out.println("color of the Classmate");
     }
 }
